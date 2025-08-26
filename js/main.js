b262f8d50be46bceb49bdb597b8a03e71bb75200
Kingrexicon <oyedeleoreofe@gmail.com>
2025-08-26 16:52:55 +0100

    added image feature to the website

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
-document.querySelector('button').addEventListener('click', apiRequest)
+document.querySelector("button").addEventListener("click", apiRequest);
 
-async function apiRequest(){
-    const rapperName = document.querySelector('input').value
-    try{
-        const response = await fetch(`https://my-first-crud.onrender.com/api/${rapperName}`)
-        const data = await response.json()
+async function apiRequest() {
+	const rapperName = document.querySelector("input").value;
+	const bodyElement = document.body;
+	try {
+		const response = await fetch(
+			`https://my-first-crud.onrender.com/api/${rapperName}`
+		);
+		const data = await response.json();
 
-        console.log(data)
-        document.querySelector('h2').innerText = data.birthName
-    }catch(error){
-        console.log(error)
-    }
+		console.log(data);
+		bodyElement.style.backgroundImage = `url('${data.imageUrl}')`;
+		bodyElement.style.backgroundSize = "cover";
+		bodyElement.style.backgroundRepeat = "no-repeat";
+		bodyElement.style.backgroundPosition = "0 center";
+		// document.querySelector("img").src = data.imageUrl;
+		document.querySelector("h2").innerHTML = `<p>
+            ${data.birthName} popularly known as ${rapperName} originates from ${data.origin}, born on the ${data.birthdate} in ${data.birthLocation}
+        </p>
+      <p> 
+         ${rapperName} is a ${data.occupation} and is into ${data.genre}  active from ${data.yearsActive} with ${data.labels} record labels.
+       </p> <p>${rapperName} has ${data.children} kids</p>`;
+	} catch (error) {
+		console.log(error);
+	}
 }
